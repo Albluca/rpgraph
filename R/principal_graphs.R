d7e56d850123e0db33a9b3ce239f56b78537c808
Albluca <luca.albergante@gmail.com>
2016-10-05 22:19:02 +0200

    Updated projecting function

--- R/principal_graphs.R
+++ R/principal_graphs.R
@@ -368,12 +368,14 @@ getTaxonMap <- function(Results, Data, UseR = FALSE){
 #' @param Data 
 #' @param TaxonList 
 #' @param UseR 
+#' @param method 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-projectPoints <- function(Results, Data, TaxonList=NULL, UseR = TRUE){
+projectPoints <- function(Results, Data, TaxonList=NULL,
+                          UseR = TRUE, method = 'Dist'){
   
   if(is.null(TaxonList)){
     print("TaxonList will be computed. Consider doing that separetedly")
@@ -386,120 +388,172 @@ projectPoints <- function(Results, Data, TaxonList=NULL, UseR = TRUE){
   
   if(UseR){
     
-    DistsLists <- list()
-    
-    for(i in 1:nrow(Results$Edges)){
+    if(method == 'OnEdges'){
       
-      Nd <- Results$Edges[i, ]
+      DistsLists <- list()
       
-      C1 <- Results$Nodes[Nd[1],]
-      C2 <- Results$Nodes[Nd[2],]
-  
-      # Select Only points associated with C1 or C2
+      for(i in 1:nrow(Results$Edges)){
+        
+        Nd <- Results$Edges[i, ]
+        
+        C1 <- Results$Nodes[Nd[1],]
+        C2 <- Results$Nodes[Nd[2],]
+        
+        # Select Only points associated with C1 or C2
+        
+        SelPoints <- c(TaxonList[[Nd[1]]], TaxonList[[Nd[2]]])
+        RestDataMap <- Data[SelPoints,]
+        
+        # plot(RestDataMap[,1:2])
+        # points(C1[1], C1[2], pch=20)
+        # points(C2[1], C2[2], pch=20)
+        # 
+        # PTOL <- NULL
+        # for(j in seq(from=0, to=1, by=0.01)){
+        #   PTOL <- rbind(PTOL, (j)*C1 + (1-j)*C2 )
+        # }
+        # points(x = PTOL[,1], y = PTOL[,2])
+        # 
+        # table(apply(as.matrix(dist(rbind(PTOL, RestDataMap)))[1:nrow(PTOL),-c(1:nrow(PTOL))], 2, which.min))
+        
+        # This is a terrible method. There must be a better way!!!
+        
+        PCARet <- prcomp(rbind(C1, C2), retx = TRUE)
+        
+        P1Pos <- PCARet$x["C1","PC1"]
+        P2Pos <- PCARet$x["C2","PC1"]
+        
+        PointPosFull <- t(t(RestDataMap) - PCARet$center) %*% PCARet$rotation
+        
+        PointPos <- PointPosFull[,"PC1"]
+        PrjPoints <- t(t(PointPos %*% t(PCARet$rotation[,1])) + PCARet$center)
+        # plot(PrjPoints[,1:2])
+        # Nodepoints <- t(t(PCARet$x[,"PC1"] %*% t(PCARet$rotation[,1])) + PCARet$center)
+        # points(Nodepoints[,1:2], col='red')
+        
+        # plot(PrjPoints[,1], PrjPoints[,2], col='red')
+        # points(x = PCARet$x[,1], y = PCARet$x[,2])
+        
+        # arrows(x0 = RestDataMap[,1], y0 = RestDataMap[,2], x1 = PrjPoints[,1], y1 = PrjPoints[,2], length = 0)
+        
+        if(P1Pos < P2Pos){
+          PointPos <- (PointPos - P1Pos) / (P2Pos-P1Pos)
+          PointPos[PointPos < 0] <- 0
+          PointPos[PointPos > 1] <- 1
+        } else {
+          PointPos <- (PointPos - P2Pos) / (P1Pos-P2Pos)
+          PointPos[PointPos < 0] <- 0
+          PointPos[PointPos > 1] <- 1
+          PointPos <- 1 - PointPos
+        }
+        
+        ElList <- list(Nodes = Nd,
+                       PointsProjections = PointPos,
+                       PointsIndices = SelPoints,
+                       SegmentDist = abs(P2Pos-P1Pos),
+                       ProjectedCoords = PrjPoints)
+        
+        DistsLists[[i]] <- ElList
+        
+      }
       
-      SelPoints <- c(TaxonList[[Nd[1]]], TaxonList[[Nd[2]]])
-      RestDataMap <- Data[SelPoints,]
+      PosVector <- matrix(rep(NA, nrow(Data)*(ncol(Data)+1)), nrow = nrow(Data))
+      RelPosVector <- matrix(rep(NA, nrow(Data)*3), nrow = nrow(Data))
+      SegLen <- NULL
+      # plot(Data[,1:2], col="gray", ylim = c(-50, 50))
+      # points(Results$Nodes[,1:2], col="black")
       
-      # plot(RestDataMap[,1:2])
-      # points(C1[1], C1[2], pch=20)
-      # points(C2[1], C2[2], pch=20)
-      # 
-      # PTOL <- NULL
-      # for(j in seq(from=0, to=1, by=0.01)){
-      #   PTOL <- rbind(PTOL, (j)*C1 + (1-j)*C2 )
+      for (i in 1:length(DistsLists)) {
+        
+        SegLen <- c(SegLen, DistsLists[[i]]$SegmentDist)
+        
+        Inside <- DistsLists[[i]]$PointsProjections > 0 & DistsLists[[i]]$PointsProjections < 1
+        
+        for (j in 1:length(DistsLists[[i]]$PointsIndices)) {
+          if(Inside[j]){
+            # The point is inside a different segment. It gets it coordinates
+            PosVector[DistsLists[[i]]$PointsIndices[j], ] <- c(1, DistsLists[[i]]$ProjectedCoords[j,])
+            RelPosVector[DistsLists[[i]]$PointsIndices[j], ] <-
+              c(DistsLists[[i]]$PointsProjections[j], DistsLists[[i]]$Nodes)
+            next()
+          }
+          
+          if(!Inside[j] & is.na(PosVector[DistsLists[[i]]$PointsIndices[j], 1])){
+            # The point is not part of a different segments.
+            # It gets the coordinate of a node
+            
+            RelPosVector[DistsLists[[i]]$PointsIndices[j], ] <-
+              c(DistsLists[[i]]$PointsProjections[j], DistsLists[[i]]$Nodes)
+            
+            if(DistsLists[[i]]$PointsProjections[j] == 0){
+              PosVector[DistsLists[[i]]$PointsIndices[j], ] <- c(0, Results$Nodes[DistsLists[[i]]$Nodes[1],])
+            }
+            if(DistsLists[[i]]$PointsProjections[j] == 1){
+              PosVector[DistsLists[[i]]$PointsIndices[j], ] <- c(0, Results$Nodes[DistsLists[[i]]$Nodes[2],])
+            }
+            next()
+          }
+          
+        }
+        
+      }
+      
+      # for (j in 1:nrow(PosVector)) {
+      #   points(x=PosVector[j,2], y=PosVector[j,3], col='green', cex=0.5)
       # }
-      # points(x = PTOL[,1], y = PTOL[,2])
-      # 
-      # table(apply(as.matrix(dist(rbind(PTOL, RestDataMap)))[1:nrow(PTOL),-c(1:nrow(PTOL))], 2, which.min))
       
-      # This is a terrible method. There must be a better way!!!
-      
-      PCARet <- prcomp(rbind(C1, C2), retx = TRUE)
-      
-      P1Pos <- PCARet$x["C1","PC1"]
-      P2Pos <- PCARet$x["C2","PC1"]
-      
-      PointPosFull <- t(t(RestDataMap) - PCARet$center) %*% PCARet$rotation
-
-      PointPos <- PointPosFull[,"PC1"]
-      PrjPoints <- t(t(PointPos %*% t(PCARet$rotation[,1])) + PCARet$center)
-      # plot(PrjPoints[,1:2])
-      # Nodepoints <- t(t(PCARet$x[,"PC1"] %*% t(PCARet$rotation[,1])) + PCARet$center)
-      # points(Nodepoints[,1:2], col='red')
-      
-      # plot(PrjPoints[,1], PrjPoints[,2], col='red')
-      # points(x = PCARet$x[,1], y = PCARet$x[,2])
-      
-      # arrows(x0 = RestDataMap[,1], y0 = RestDataMap[,2], x1 = PrjPoints[,1], y1 = PrjPoints[,2], length = 0)
-      
-      if(P1Pos < P2Pos){
-        PointPos <- (PointPos - P1Pos) / (P2Pos-P1Pos)
-        PointPos[PointPos < 0] <- 0
-        PointPos[PointPos > 1] <- 1
-      } else {
-        PointPos <- (PointPos - P2Pos) / (P1Pos-P2Pos)
-        PointPos[PointPos < 0] <- 0
-        PointPos[PointPos > 1] <- 1
-        PointPos <- 1 - PointPos
-      }
-
-      ElList <- list(Nodes = Nd,
-                     PointsProjections = PointPos,
-                     PointsIndices = SelPoints,
-                     SegmentDist = abs(P2Pos-P1Pos),
-                     ProjectedCoords = PrjPoints)
-      
-      DistsLists[[i]] <- ElList
+      return(list(PointsOnEdgesCoords = PosVector, EdgeLength = SegLen, PointsOnEdgesDist = RelPosVector))
       
     }
     
-    PosVector <- matrix(rep(NA, nrow(Data)*(ncol(Data)+1)), nrow = nrow(Data))
-    RelPosVector <- matrix(rep(NA, nrow(Data)*3), nrow = nrow(Data))
-    SegLen <- NULL
-    # plot(Data[,1:2], col="gray", ylim = c(-50, 50))
-    # points(Results$Nodes[,1:2], col="black")
-
-    for (i in 1:length(DistsLists)) {
- 
-      SegLen <- c(SegLen, DistsLists[[i]]$SegmentDist)
+    if(method == 'Dist'){
       
-      Inside <- DistsLists[[i]]$PointsProjections > 0 & DistsLists[[i]]$PointsProjections < 1
+      FullMat <- rbind(Results$Nodes, Data)
+      DistMat <- as.matrix(dist(FullMat))
       
-      for (j in 1:length(DistsLists[[i]]$PointsIndices)) {
-        if(Inside[j]){
-          # The point is inside a different segment. It gets it coordinates
-          PosVector[DistsLists[[i]]$PointsIndices[j], ] <- c(1, DistsLists[[i]]$ProjectedCoords[j,])
-          RelPosVector[DistsLists[[i]]$PointsIndices[j], ] <-
-            c(DistsLists[[i]]$PointsProjections[j], DistsLists[[i]]$Nodes)
-          next()
+      RedDistMat <- DistMat[1:nrow(Results$Nodes), -c(1:nrow(Results$Nodes))]
+      NodeDistMat <- DistMat[1:nrow(Results$Nodes), c(1:nrow(Results$Nodes))]
+      
+      if(min(Results$Edges)==0){
+        Results$Edges <- Results$Edges + 1
+      }
+      
+      PosVector <- matrix(rep(NA, nrow(Data)*(ncol(Data)+1)), nrow = nrow(Data))
+      RelPosVector <- matrix(rep(NA, nrow(Data)*3), nrow = nrow(Data))
+      SegLen <- NULL
+      
+      SortedNodes <- apply(RedDistMat, 2, sort, index.return=TRUE)
+      for (i in 1:length(SortedNodes)) {
+        BaseNode <- SortedNodes[[i]]$ix[1]
+        PossibleNodes <- unique(as.vector(Results$Edges[which(rowSums(Results$Edges == BaseNode)>0),]))
+        PossibleNodes <- setdiff(PossibleNodes, BaseNode)
+        SecondaryNodeIdx <- min(which(SortedNodes[[i]]$ix %in% PossibleNodes))
+        SecondaryNode <- SortedNodes[[i]]$ix[SecondaryNodeIdx]
+        
+        EdgeId <- which(rowSums(matrix(Results$Edges %in% c(BaseNode, SecondaryNode), ncol = 2)) == 2)
+        if(Results$Edges[EdgeId, 1] == BaseNode){
+          PointDists <- SortedNodes[[i]]$x[c(1, SecondaryNodeIdx)]
+          RelPosVector[i,] <- c(PointDists[1]/sum(PointDists), BaseNode, SecondaryNode)
+          PosVector[i, ] <- c(1, RelPosVector[i,1]*Results$Nodes[BaseNode,] +
+                                (1-RelPosVector[i,1])*Results$Nodes[SecondaryNode,])
+        } else {
+          PointDists <- SortedNodes[[i]]$x[c(1, SecondaryNodeIdx)]
+          RelPosVector[i,] <- c(PointDists[2]/sum(PointDists), SecondaryNode, BaseNode)
+          PosVector[i, ] <- c(1, RelPosVector[i,1]*Results$Nodes[SecondaryNode,] +
+                                (1-RelPosVector[i,1])*Results$Nodes[BaseNode,])
         }
         
-        if(!Inside[j] & is.na(PosVector[DistsLists[[i]]$PointsIndices[j], 1])){
-          # The point is not part of a different segments.
-          # It gets the coordinate of a node
-          
-          RelPosVector[DistsLists[[i]]$PointsIndices[j], ] <-
-            c(DistsLists[[i]]$PointsProjections[j], DistsLists[[i]]$Nodes)
-          
-          if(DistsLists[[i]]$PointsProjections[j] == 0){
-            PosVector[DistsLists[[i]]$PointsIndices[j], ] <- c(0, Results$Nodes[DistsLists[[i]]$Nodes[1],])
-          }
-          if(DistsLists[[i]]$PointsProjections[j] == 1){
-            PosVector[DistsLists[[i]]$PointsIndices[j], ] <- c(0, Results$Nodes[DistsLists[[i]]$Nodes[2],])
-          }
-          next()
-        }
+        SegLen <- c(SegLen, NodeDistMat[BaseNode, SecondaryNode])
         
       }
-
+      
+      return(list(PointsOnEdgesCoords = PosVector, EdgeLength = SegLen, PointsOnEdgesDist = RelPosVector))
+      
     }
     
-    # for (j in 1:nrow(PosVector)) {
-    #   points(x=PosVector[j,2], y=PosVector[j,3], col='green', cex=0.5)
-    # }
- 
-    return(list(PointsOnEdgesCoords = PosVector, EdgeLength = SegLen, PointsOnEdgesDist = RelPosVector))
-    
+    print("Method not supported")
+    return(NULL)
+       
   } else {
     
     print("Sorry. Not implemented yet ...")
