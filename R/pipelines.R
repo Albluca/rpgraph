ea6fa5e3d63fe5c197e15858c1a0b49722278adf
Albluca <luca.albergante@gmail.com>
2016-11-02 13:55:23 +0100

    fixed heatmaping

--- R/pipelines.R
+++ R/pipelines.R
@@ -463,7 +463,7 @@ StudyCellCycles <- function(ExpressionMatrix, Grouping, GeneSet = NULL,
   
   print(table(DF.Plot$Grouping, DF.Plot$Stage))
   
-  if(Interactive){
+  if(Interactive & length(unique(Grouping)) > 1){
     gplots::heatmap.2(table(DF.Plot$Grouping, DF.Plot$Stage))
   }
   
